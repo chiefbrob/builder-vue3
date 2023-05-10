c9d94adce4e22d6648879c24696ef7e91056ade6
Brian Chief Obare <brianobare@gmail.com>
2023-05-10 12:25:53 +0300

    cleaner

--- src/components/shared/dev/CodeEditor.vue
+++ src/components/shared/dev/CodeEditor.vue
@@ -1,27 +1,32 @@
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 
 let textarea = ref(null)
 
-onMounted(() => {
-  textarea.value.addEventListener('keydown', (e) => {
-    let t = textarea.value
-    if (e.keyCode === 9) {
-      let val = t.value,
-        start = t.selectionStart,
-        end = t.selectionEnd
+function onkeydown(e: Event) {
+  let t = textarea.value
 
-      t.value = val.substring(0, start) + '\t' + val.substring(end)
+  let val = t.value,
+    start = t.selectionStart,
+    end = t.selectionEnd
 
-      t.selectionStart = t.selectionStart + ' '
-      e.preventDefault()
-    }
-  })
-})
+  t.value = val.substring(0, start) + '\t' + val.substring(end)
+
+  t.selectionStart = t.selectionStart + ' '
+  e.preventDefault()
+}
 </script>
 
 <template>
-  <textarea name="" id="" ref="textarea" placeholder="type me" cols="30" rows="10">
+  <textarea
+    name=""
+    id=""
+    ref="textarea"
+    @keydown.tab="onkeydown"
+    placeholder="type me"
+    cols="30"
+    rows="10"
+  >
 Hi there</textarea
   >
 </template>
