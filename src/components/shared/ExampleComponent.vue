7c343c29f454e120b196e4b3929373c61a60e871
Brian Chief Obare <brianobare@gmail.com>
2023-08-07 00:03:24 +0300

    bootstrap works

---
+++ src/components/shared/ExampleComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import useStorage from '@/composables/storage/useStorage'
+import CodeEditor from '@/components/shared/dev/CodeEditor.vue'
+import { ref } from 'vue'
+import { useCounterStore } from '@/stores/useCounterStore'
+
+let food = useStorage('food', 'salad')
+let age = useStorage('age')
+let comment = ref('foo bar')
+
+let counter = useCounterStore()
+</script>
+
+<template>
+  <div>
+    <h4>Counter {{ counter.count }}</h4>
+    <p>What is your favorite food? <input type="text" v-model="food" /></p>
+    <p>How old are you? <input type="number" v-model="age" /></p>
+    <form>
+      <code-editor v-model="comment" cols="30" rows="10" />
+    </form>
+    <p>
+      <button @click="counter.increment">Increment</button>
+      <button>{{ counter.doubleCount }}</button>
+    </p>
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import './assets/scss/style.scss'
+import 'bootstrap'
+
 import App from './App.vue'
 import router from './router'
 
