3ec4e652b504730803be9e0a52bb11e92db80ae1
Brian Obare <brianobare@gmail.com>
2023-12-04 20:38:09 +0300

    grooming

--- src/components/nav/BasicNav.vue
+++ src/components/nav/BasicNav.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import useDeepStorage from '@/composables/storage/useDeepStorage'
 import logout from '@/scripts'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const open = (name: string) => {
+  router.push({ name: name })
+}
 const user = useDeepStorage('user')
 </script>
 
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#" @click="$router.push({ name: 'welcome' })">Builder-vue3</a>
+      <a class="navbar-brand" href="#" @click="open('welcome')">Builder-vue3</a>
       <button
         class="navbar-toggler"
         type="button"
@@ -21,18 +27,15 @@ const user = useDeepStorage('user')
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item" @click="$router.push({ name: 'about' })">
+          <li class="nav-item" @click="open('about')">
             <a class="nav-link" :active="$route.name === 'about'" aria-current="page" href="#"
               >About</a
             >
           </li>
-          <li class="nav-item" @click="$router.push({ name: 'home' })">
+          <li class="nav-item" @click="open('home')">
             <a class="nav-link" :active="$route.name === 'home'" aria-current="page" href="#"
               >Home</a
             >
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
           </li>
           <li class="nav-item dropdown" v-if="user === null">
             <a
@@ -45,14 +48,14 @@ const user = useDeepStorage('user')
               Account
             </a>
             <ul class="dropdown-menu">
-              <li @click="$router.push({ name: 'login' })">
+              <li @click="open('login')">
                 <a class="dropdown-item" href="#">Login</a>
               </li>
-              <li @click="$router.push({ name: 'register' })">
+              <li @click="open('register')">
                 <a class="dropdown-item" href="#">Register</a>
               </li>
               <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#">Reset Account</a></li>
             </ul>
           </li>
           <li class="nav-item dropdown" v-else>
@@ -66,13 +69,13 @@ const user = useDeepStorage('user')
               {{ user.username }}
             </a>
             <ul class="dropdown-menu">
-              <li @click="$router.push({ name: 'profile' })">
+              <li @click="open('profile')">
                 <a class="dropdown-item" href="#">{{ user.name }}</a>
               </li>
-              <li @click="$router.push({ name: 'settings' })">
+              <li @click="open('settings')">
                 <a class="dropdown-item" href="#">Settings</a>
               </li>
-              <li @click="$router.push({ name: 'contacts' })">
+              <li @click="open('contacts')">
                 <a class="dropdown-item" href="#">Feedback</a>
               </li>
               <li><hr class="dropdown-divider" /></li>
@@ -80,17 +83,11 @@ const user = useDeepStorage('user')
             </ul>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#" @click="$router.push('/contact')"
-              >Contact</a
-            >
+            <a class="nav-link" aria-current="page" href="#" @click="open('contact')">Contact</a>
           </li>
         </ul>
         <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
     </div>
